23ca44fe07a719cf292388b7422f3db1c03ccb8b
Abdillah Bakari <abdibaker1@gmail.com>
2022-04-22 17:21:03 +0300

    ☮  Add customized error milddleware

---
+++ auth/src/middlewares/error-hadle.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log('something went wrong', err);
+  res.status(400).send('something went wrong');
+};
