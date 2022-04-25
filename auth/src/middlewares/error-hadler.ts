b75e16651ddb65b04e8a92770088a9bf540acf92
Abdillah Bakari <abdibaker1@gmail.com>
2022-04-25 16:54:05 +0300

    Add custom error class

--- auth/src/middlewares/error-hadler.ts
+++ auth/src/middlewares/error-hadler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,6 +7,9 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('something went wrong', err);
-  res.status(400).send('something went wrong');
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeError() });
+  }
+
+  res.status(400).send({ errors: [{ message: 'Something went wrong' }] });
 };
