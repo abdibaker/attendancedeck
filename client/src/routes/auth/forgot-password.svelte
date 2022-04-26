b4403c3004ebd20af6b1db339e7903e434897155
umama <harnekarumama110@gmail.com>
2022-04-26 12:00:37 +0300

    migrate from svelte to vue with Nuxtjs 3 framework

--- client/src/routes/auth/forgot-password.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { sTitle } from './store';
-
-	sTitle.set('Reset your password');
-</script>
-
-<div class="form-control mt-4 mb-2">
-	<button class="btn btn-primary">Login</button>
-</div>
